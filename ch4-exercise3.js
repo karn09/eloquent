653166109c7d341f1a597cd6242f9888bc159491
karn09 <karn09@gmail.com>
2014-09-27 09:23:13 -0400

    added prepend and nth function

--- ch4-exercise3.js
+++ ch4-exercise3.js
@@ -1,9 +1,9 @@
 // Your code here.
 
 var arrayToList = function(array) {
-  var next = null;
+  var list = null;
   for (var i = array.length - 1; i >= 0; i--) {
-    var list = {
+    list = {
       value: array[i],
       next: list
     };
@@ -19,19 +19,26 @@ var listToArray = function(list) {
   return array;
 };
 
-var prepend = function(value) {
-
+var prepend = function(value, list) {
+  return {
+    value: value,
+    next: list
+  };
 };
 
-var nth = function(list, step) {
-
+var nth = function(list, n) {
+  if (n === 0) {
+    return list.value;
+  } else {
+    return nth(list.next, n - 1);
+  }
 };
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-//console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
