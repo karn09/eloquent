107613ca41ccf8aed9f6c9e222aad3dbf06d2d27
karn09 <karn09@gmail.com>
2014-09-27 08:06:36 -0400

    Fixed listToArray function

--- ch4-exercise3.js
+++ ch4-exercise3.js
@@ -1,31 +1,33 @@
 // Your code here.
 
 var arrayToList = function(array) {
-  //var next = null;
   var next = null;
   for (var i = array.length - 1; i >= 0; i--) {
-    var list = {value: array[i], next: list}
+    var list = {
+      value: array[i],
+      next: list
+    };
   }
   return list;
 };
 
 var listToArray = function(list) {
- var array = [];
- for (var node = list; node; node = node.rest) {
-   array.push(node.value);
- }
- return array;
+  var array = [];
+  for (var node = list; node; node = node.next) {
+    array.push(node.value);
+  }
+  return array;
 };
 
 var prepend = function(value) {
 
 };
 
-var nth = function (list, step) {
+var nth = function(list, step) {
 
 };
 
-//console.log(arrayToList([10, 20]));
+console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
